52eb60db46431fb9ca6e85403848527b63c94ab6
hernan <hernan.hnet@gmail.com>
2018-02-28 15:37:02 -0300

    RxJs Part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class LeaderService {
