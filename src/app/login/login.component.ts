7ed768826881ef37e43e519adf14d4dd5aa6f4ca
hlavrencic <hernan.hnet@gmail.com>
2020-12-17 01:39:38 -0300

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,8 @@ export class LoginComponent implements OnInit {
 
   user = {
     remember: false,
+    username: '',
+    password: ''
   };
 
   constructor(public dialogRef: MatDialogRef<LoginComponent>) { }
